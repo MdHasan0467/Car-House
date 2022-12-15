b624d4eb66aac6b21f828c36d7d2c9aa59d7377c
MdHasan0467 <ornilhasan.oht.riyad@gmail.com>
2022-12-16 01:21:28 +0600

    payment route create

--- src/components/Shared/DashBoard/Payment/Payment.js
+++ src/components/Shared/DashBoard/Payment/Payment.js
@@ -2,13 +2,22 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Payment = () => {
-	const bookings = useLoaderData()
+	const bookings = useLoaderData();
 	console.log(bookings);
-    return (
-			<div>
-				<h1>Payment Now</h1>
-			</div>
-		);
+	const { productCategory, productModel, resellPrice } = bookings;
+	return (
+		<div>
+			<h1>Payment Now</h1>
+			<h2 className='text-start ml-10 text-xl'>
+				Please pay : <span className='text-blue-600 font-bold'> ${resellPrice}</span>
+			</h2>
+			<h2 className='text-start ml-10'>For,</h2>
+			<h2 className='text-start ml-10'>
+				<span className='mr-3 text-2xl'>Brand : {productCategory} , </span>
+				<span className='mr-3 text-xl'>Model : {productModel}</span>
+			</h2>
+		</div>
+	);
 };
 
 export default Payment;
